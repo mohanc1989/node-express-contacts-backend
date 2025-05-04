b81d997fefecbf4bd6180060fb275b8bdda56774
mohan-FH <mohan.c@foodhub.com>
2025-05-04 17:17:38 +0530

    improved route

--- route/contactRoutes.js
+++ route/contactRoutes.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {getContacts,creatContact,updateContact,deleteContact,getContact} = require("../controller/contactController")
 
-router.route("/").get(getContacts);
-router.route("/").post(creatContact);
-router.route("/:id").put(updateContact);
-router.route("/:id").delete(deleteContact);
-router.route("/:id").get(getContact);
-
+router.route("/").get(getContacts).post(creatContact);
+router.route("/:id").put(updateContact).delete(deleteContact).get(getContact);
 module.exports = router;
