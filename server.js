c2fa00ee13794e3dbf6ada443323003097309997
mohan-FH <mohan.c@foodhub.com>
2025-05-04 17:16:05 +0530

    mongo db connect added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+connectDb();
 app.use(express.json())
 app.use("/api/contacts", require("./route/contactRoutes"));
 app.use(errorHandler);
