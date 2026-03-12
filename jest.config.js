687df28cf497ebe466d4aa541435d02e884387fb
mohan-FH <mohan.c@foodhub.com>
2026-03-12 12:20:53 +0530

    test converage config updated

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,14 @@ module.exports = {
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   testTimeout: 10000
 }; 
